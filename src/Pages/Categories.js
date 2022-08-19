33b0a74ba88137e62eb2daf685fa7b51d3afcf33
shahira-sadat <shahira.sadat1@gmail.com>
2022-08-19 05:15:29 +0430

    Add react-icons

--- src/Pages/Categories.js
+++ src/Pages/Categories.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { checkStatus } from '../redux/categories/categories';
+import { IoConstructOutline } from 'react-icons/io5';
 
 function Categories() {
   const categories = useSelector((state) => state.categories);
   const dispatch = useDispatch();
   return (
     <div className="categories-container">
+      <IoConstructOutline className="construction-icon" />
       <h2>{categories}</h2>
       <button
         onClick={() => {
